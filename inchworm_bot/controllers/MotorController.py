fdbf19da485bb66327c21d2d87aa40022f89b630
Chandler Garcia <crgarcia@wpi.edu>
2022-11-30 12:15:13 -0500

    Added a motor controller and webots servo controller handler, can use it in the future to work on motor stuff

--- inchworm_bot/controllers/MotorController.py
+++ inchworm_bot/controllers/MotorController.py
@@ -1,23 +1,28 @@
 import Manipulator
 import math as math
+from WebotsServoController import WebotsServoController
+
 class MotorController:
     def __init__(self):
         """
         each motor has a current start position and a max and min that they can turn
         The order goes
-        Min, Max, Start Position
+        Start Position
+        Limiters
         """
         self.a = 0
         self.b = 0
         self.c = 0
         self.d = 0
         self.e = 0
+
         self.motor1 = [-math.pi, math.pi]
         self.motor2 = [-math.pi, math.pi]
         self.motor3 = [-math.pi, math.pi]
         self.motor4 = [-math.pi, math.pi]
         self.motor5 = [-math.pi, math.pi]
         self.manipulator = Manipulator
+        self.WebotsController = WebotsServoController()
 
     def moveToPoint(self, x, y, z):
         joint_angles = self.manipulator.InchwormIK(x, y, z, self.a, self.b, self.c, self.d, self.e)
---
+++ inchworm_bot/controllers/WebotsServoController.py
@@ -0,0 +1,34 @@
+from time import time
+try:
+    from controller import Robot as Webot
+except:
+    print("\nWARNING: Unable to import Webots library. Continuing without Webots functionality.")
+    class Webot:
+        def getDevice(self, jointName):
+            return None
+
+class WebotsServoController:
+    def __init__(self):
+        self.webot = Webot()
+        self.simStep = int(self.webot.getBasicTimeStep())
+        self.reset()
+
+    def reset(self):
+        self.simTime = int(0)
+        self.startTime = time()
+
+    def update(self):
+        self.simTime = self.simTime + self.simStep if self.simStep > 0 else round(
+            1000 * (time() - self.startTime))
+        return self.webot.step(self.simStep) == -1
+
+    def addServo(self, jointName):
+        s = self.webot.getDevice(jointName)
+        if s is not None:
+            s.setPosition(0)
+        return s
+        def getBasicTimeStep(self):
+            return 0
+
+        def step(self, timeStep):
+            return False
