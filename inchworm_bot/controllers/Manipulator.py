420f5aeba6cf30987e26abf95404d18bd84947aa
Chandler Garcia <crgarcia@wpi.edu>
2022-11-30 15:44:29 -0500

    Might have the correct Hinge Joint names, unsure

--- inchworm_bot/controllers/Manipulator.py
+++ inchworm_bot/controllers/Manipulator.py
@@ -21,19 +21,20 @@ class Manipulator:
         self.eng = matlab.engine.start_matlab() # Makes my life so much easier, no need to create new FK and IK scripts
         self.eng.addpath(r'/home/midnightpegasus/Documents/WPI/RobotDynamics/Final_Project/RBE501-Inchworm/inchworm_bot/controllers',nargout=5)
 
-    def InchwormFK(self, a, b, c, d, e):
+    def InchwormFK(self, Joints):
         """
-        Takes in all 5 joint angles and returns the foot position
-        :param a:
-        :param b:
-        :param c:
-        :param d:
-        :param e:
+        Takes in all 5 joint angles and returns the foot position, Joints is a [] of Servos currently
+        :param Joints: The joints of the robot, in order
         :return:
         """
+        a = Joints[0].getPosition()
+        b = Joints[1].getPosition()
+        c = Joints[2].getPosition()
+        d = Joints[3].getPosition()
+        e = Joints[4].getPosition()
         T = self.eng.FK_Inchworm(a,b,c,d,e)
 
-    def InchwormIK(self,x,y,z,a,b,c,d,e,alpha,beta,gamma):
+    def InchwormIK(self,x,y,z,Joints,alpha,beta,gamma):
         """
         xyz coordinates we want to go
         :param x:
@@ -41,11 +42,7 @@ class Manipulator:
         :param z:
 
         Initial position of the arm (current Q)
-        :param a:
-        :param b:
-        :param c:
-        :param d:
-        :param e:
+        :param Joints: The joints of the robot, in order
 
         xyz Euler coordinates of final position we want
         :param alpha:
@@ -53,10 +50,15 @@ class Manipulator:
         :param gamma:
         :return:
         """
+        a = Joints[0].getPosition()
+        b = Joints[1].getPosition()
+        c = Joints[2].getPosition()
+        d = Joints[3].getPosition()
+        e = Joints[4].getPosition()
         IK = self.eng.IK_Inchworm(x,y,z,a,b,c,d,e,alpha,beta,gamma)
         return IK # Might need to change this return type to be a list instead of what it might be - maybe a string, unsure
 
-    def InchwormIK(self,x,y,z,a,b,c,d,e):
+    def InchwormIK(self,x,y,z,Joints):
         """
         Go to point, final orientation doesn't matter
         xyz coordinates we want to go
@@ -65,11 +67,12 @@ class Manipulator:
         :param z:
 
         Initial position of the arm (current Q)
-        :param a:
-        :param b:
-        :param c:
-        :param d:
-        :param e:
+        :param Joints: The joints of the robot, in order
         """
+        a = Joints[0].getPosition()
+        b = Joints[1].getPosition()
+        c = Joints[2].getPosition()
+        d = Joints[3].getPosition()
+        e = Joints[4].getPosition()
         IK = self.eng.IK_InchwormNoOrientation(x,y,z,a,b,c,d,e)
         return IK # Might need to change this return type to be a list instead of what it might be - maybe a string, unsure
--- inchworm_bot/controllers/MotorController.py
+++ inchworm_bot/controllers/MotorController.py
@@ -13,15 +13,15 @@ class MotorController:
         self.manipulator = Manipulator
         self.WebotsController = WebotsServoController()
 
-        joint_a = Servo(webots_servo=self.WebotsController.addServo("Need Names Here"))
-        joint_b = Servo(webots_servo=self.WebotsController.addServo("Need Names Here"))
-        joint_c = Servo(webots_servo=self.WebotsController.addServo("Need Names Here"))
-        joint_d = Servo(webots_servo=self.WebotsController.addServo("Need Names Here"))
-        joint_e = Servo(webots_servo=self.WebotsController.addServo("Need Names Here"))
+        joint_a = Servo(webots_servo=self.WebotsController.addServo("foot_ankle_joint2"))
+        joint_b = Servo(webots_servo=self.WebotsController.addServo("ankle_leg_joint2"))
+        joint_c = Servo(webots_servo=self.WebotsController.addServo("center_joint"))
+        joint_d = Servo(webots_servo=self.WebotsController.addServo("leg_ankle_joint1"))
+        joint_e = Servo(webots_servo=self.WebotsController.addServo("ankle_foot_joint1"))
         self.Joints = [joint_a, joint_b, joint_c, joint_d, joint_e]
 
     def moveToPoint(self, x, y, z):
-        joint_angles = self.manipulator.InchwormIK(x, y, z, self.a, self.b, self.c, self.d, self.e)
+        joint_angles = self.manipulator.InchwormIK(x, y, z, self.Joints)
         # We can either stick this directly into webots, and it will go here based off the max speed the motors can go,
         # OR we make actual trajeectory planning
 
