325eb92f04771efe7b1ef322b6a92be5c441aa3e
Chandler Garcia <crgarcia@wpi.edu>
2022-12-12 21:34:15 -0500

    Created the Ss, testing FK, IK, and IK inchworm no orientaiton

--- inchworm_bot/controllers/FK_Inchworm.m
+++ inchworm_bot/controllers/FK_Inchworm.m
@@ -1,7 +1,14 @@
 function T = FK_Inchworm(a,b,c,d,e)
     %abcde are the joint angles for the inchworm
-    S = [];
-    M = [];
+    S = [0 0 1 0 0 0;
+        0 1 0 109.03 0 0;
+        0 1 0 248.4913 -84.9940 0;
+        0 1 0 109.03 -169.9879 0;
+        0 0 1 0 -169.9879 0];
+    M = [1 0 0 169.9879;
+        0 -1 0 0;
+        0 0 -1 0;
+        0 0 0 1];
     T = 1;
     q = [a b c d e];
     for i = 1:length(S(1,:))
