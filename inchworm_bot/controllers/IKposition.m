d48f5a6524c9db6d305361abd21723ac89548c13
Chandler Garcia <crgarcia@wpi.edu>
2022-12-14 03:43:43 -0500

    Hustling trying to make it work

--- inchworm_bot/controllers/IKposition.m
+++ inchworm_bot/controllers/IKposition.m
@@ -6,25 +6,28 @@ function q = IKposition(S,M,currentT,targetT,currentQ)
     currentR = currentT(1:3,1:3);
     k = 0.1;
     
-    while norm(targetPose - currentPose) + norm(targetR - currentR) > 1e-3
+%     while norm(targetPose - currentPose) + norm(targetR - currentR) > 1e-3
+    while norm(targetPose - currentPose) > 1e-3
+%         J_a = jacob0(S,currentQ);
         J_a = jacoba(S,M,currentQ);
         
         % Use the Levenberg-Marquadt algorithm (Damped Least Squares)
         lambda = 0.5;
-        rotation_error = targetR - currentR
-        poseError = (targetPose - currentPose)
-        theta = acos((cos(rotation_error(1,1)+cos(rotation_error(2,2))+cos(rotation_error(3,3)))-1)/2);
-        n = 1/(2*sin(theta))*[cos(rotation_error(3,2))-cos(rotation_error(2,3));cos(rotation_error(1,3))-cos(rotation_error(3,1));cos(rotation_error(2,1))-cos(rotation_error(1,2))];
-        phi = theta*n;
-        J = jacob0(S,currentQ);
-        deltaQ_Rotation = J_a' * pinv(J_a * J_a') * phi;
-        deltaQ = J_a' * pinv(J_a*J_a' + lambda^2 * eye(3)) * poseError + deltaQ_Rotation;
+%         rotation_error = targetR - currentR
+%         theta = acos((cos(rotation_error(1,1)+cos(rotation_error(2,2))+cos(rotation_error(3,3)))-1)/2);
+%         n = 1/(2*sin(theta))*[cos(rotation_error(3,2))-cos(rotation_error(2,3));cos(rotation_error(1,3))-cos(rotation_error(3,1));cos(rotation_error(2,1))-cos(rotation_error(1,2))];
+%         phi = theta*n;
+%         J = jacob0(S,currentQ);
+%         deltaQ_Rotation = J_a' * pinv(J_a * J_a') * phi;
+%         deltaQ = J_a' * pinv(J_a*J_a' + lambda^2 * eye(3)) * poseError + deltaQ_Rotation;
+        deltaQ = J_a' * pinv(J_a*J_a' + lambda^2 * eye(3)) * (targetPose - currentPose);
+%         deltaQ = J_a\(targetPose - currentPose)
                     
         currentQ = currentQ + deltaQ';
         T = fkine(S,M,currentQ,'space');
-        currentPose = T(1:3,4);
-        currentR = T(1:3,1:3)
-        targetR
+        currentPose = T(1:3,4)
+%         currentR = T(1:3,1:3)
+%         targetR
     end
     q = currentQ;
 end
--- inchworm_bot/controllers/TestRobot.py
+++ inchworm_bot/controllers/TestRobot.py
@@ -1,21 +1,23 @@
+import numpy as np
+
 from MotorController import MotorController
 from Manipulator import Manipulator
 from math import pi
-Time_Step = 2
+Time_Step = 16
 
 def main():
     print("Starting Inchworm Running")
     motorController = MotorController()
     man = Manipulator()
+    Joints = [0.1789,0.2135,-0.1146,-0.1252,-0.1576]
     # man = Manipulator()
     # Joints = [0,0,0,0,0]
     # man.InchwormFKNoWebots(Joints)
     # motorController.whereAt()
     i = 0
     while motorController.robot.step(Time_Step) != -1:
-        Joints = [i * 2 * pi / 100, 0, 0, 0, 0]
         J = [0, 0, 0, 0, 0]
-        T = man.InchwormFK(Joints)
+        # T = man.InchwormFK(Joints)
         # q = man.InchwormIK(T, J)
         motorController.move(Joints)
         print(f"Given {Joints}")
